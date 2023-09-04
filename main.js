3734314d6d412543982c989ffeec66dcdbd32c4f
Vladimir Maximov <m.a.x.i.m.o.v@yandex.ru>
2023-09-04 13:59:54 +0300

    включение и тест скрипта предзагрузки, создание директорий для процессов electron

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 1200,
-        height: 800,
+        // width: 1200,
+        // height: 800,
         webPreferences: {
-            //
+            preload: path.join(__dirname, 'preload.js')
         }
     });
 
--- process_renderer/js/index.js
+++ process_renderer/js/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import '../css/style.css';
+
+import { App } from "./App";
 
 const root = createRoot(document.getElementById('root'));
-root.render(<h1>Hello React!!!</h1>);
+root.render(<App/>);
