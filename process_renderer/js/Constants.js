8dc2ad707af97f4be70d53bc1086bb4499de7708
Vladimir Maximov <m.a.x.i.m.o.v@yandex.ru>
2023-09-11 15:29:35 +0300

    правка константы распределения финансов процесса рендеринга

--- process_renderer/js/Constants.js
+++ process_renderer/js/Constants.js
@@ -1,2 +1,2 @@
-export const DISTRIBUTION_OF_FINANCES = 'distribution 0f finances';
+export const DISTRIBUTION_OF_FINANCES = 'distribution of finances';
 export const EXPENSES_CATEGORIES = 'expenses categories';
